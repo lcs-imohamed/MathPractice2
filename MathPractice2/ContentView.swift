d70106cfdebb41f98f93c3b6e0fc3c9c8146aad6
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-13 16:01:42 -0500

    Created properties to represent user guess and parts of the multiplication question. Also added question and result field to interface.

--- MathPractice2/ContentView.swift
+++ MathPractice2/ContentView.swift
@@ -9,21 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //MARK: Properties
+    
     //Create variable to store user guess
+    
     @State var guess: String = ""
     
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Guess")
-            TextField("Enter Guess Here", text: $guess )
-        }
-        .padding()
+    //Create variables for different parts of the mulitplication question
+    
+    @State var multiplier: Int = 0
+    @State var multiplicand: Int = 2
         
+    var body: some View {
+        
+        VStack {
+            Text("Question: \(multiplier) x \(multiplicand) = ?")
+                .padding()
+            Text("Result:")
+                .padding()
+            Text("Guess:")
+                .padding()
+            TextField("Enter Guess Here", text: $guess )
+                
+                //Add button to submit user guess
+                //            Button("Submit Guess") {
+                //                var guess
+                //            }
+                //        }
+                .padding()
+            
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
